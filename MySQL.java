7d22fa0252ac02360f89269946d257d1a9620148
Mr.Blackwell <ivanov.valentin2030@gmail.com>
2015-11-28 02:24:59 +0300

    add

--- MySQL.java
+++ MySQL.java
@@ -3,33 +3,31 @@ import java.sql.*;
 
 /**
  * Created by Валентин on 28.11.2015.
- * Тестовый класс, для проверки подключения к БД.
  */
-public class Connect {
-    public static void main (String[] args)
-    {
+public class MySQL {
+    public Connection connectDataBase() {
         Connection conn = null;
-        try
-        {
+        try {
             String userName = "root";
             String password = "110995";
             String url = "jdbc:mysql://localhost/test";
-            Class.forName ("com.mysql.jdbc.Driver").newInstance ();
-            conn = DriverManager.getConnection (url, userName, password);
-            System.out.println ("Database connection established");
-        }
-        catch (Exception e)
-        {
-            System.err.println ("Cannot connect to database server");
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("Database connection established");
+            return conn;
+        } catch (Exception e) {
+            System.err.println("Cannot connect to database server");
             e.printStackTrace();
+            return null;
         }
-        finally
+    }
+    public void disconnectDataBase(Connection conn) {
         {
             if (conn != null)
             {
                 try
                 {
-                    conn.close ();
+                    conn.close();
                     System.out.println ("Database connection terminated");
                 }
                 catch (Exception e) { }
